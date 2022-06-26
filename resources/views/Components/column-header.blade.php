80561b69f0f683331833814895b7fc6cdd297a9c
Dylan-Buras <dylan.buras@gmail.com>
2022-06-26 15:08:16 -0500

    fuleQuoteHistory

---
+++ resources/views/Components/column-header.blade.php
@@ -0,0 +1,5 @@
+<div class = "flex space-x-4 items-center">
+    <a href="#">
+        <span>{{$slot}}</span>
+    </a>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    // return view('profileManagement');
+    //return view('fuelQuoteHistory');
+    //return view('profileManagement');
     // return view('fuelQuoteForm');
     return view('welcome');
 });
