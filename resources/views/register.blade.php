a06ac377f47f4ae9b1ed4b93c98a27f80e4faf60
Michelle Cao <89444557+av0caod0@users.noreply.github.com>
2022-07-24 19:43:35 -0500

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,32 +1,9 @@
-
-
 <!DOCTYPE html>
 <html>
     <head>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="team-project/public/css/login.css">
-    </head>
-
-    <?php
-    
-    session_start();
-
-    if(isset($_POST['submit'])){
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $confirmPassword = $_POST['confirmPassword'];
-
-        if($password == $confirmPassword){
-            $result = DB::insert('INSERT INTO UserCredentials (user_id, password) 
-                    VALUES (?, ?)', [$username, $password]);
-            echo "<script>alert('You have successfully registered!')</script>";
-        }
-        else{
-            echo "<script>alert('Password does not match. Try again!')</script>";
-        }
-    }
-
-?>
+        <link rel="stylesheet" href="login.css">
+    </head>  
 
     <body>
         <div class="login-div">
@@ -37,8 +14,8 @@
             <div class="title">REGISTER</div>
 
             <div class="form">
-                <form action="" method="POST" class=register>
-
+                <form action="\registerSubmit" method="POST" class=register>
+                    @csrf
                 <div class="username">
                     <svg class="svg-icon" viewBox="0 0 20 20">
                         <path d="M12.075,10.812c1.358-0.853,2.242-2.507,2.242-4.037c0-2.181-1.795-4.618-4.198-4.618S5.921,4.594,5.921,6.775c0,1.53,0.884,3.185,2.242,4.037c-3.222,0.865-5.6,3.807-5.6,7.298c0,0.23,0.189,0.42,0.42,0.42h14.273c0.23,0,0.42-0.189,0.42-0.42C17.676,14.619,15.297,11.677,12.075,10.812 M6.761,6.775c0-2.162,1.773-3.778,3.358-3.778s3.359,1.616,3.359,3.778c0,2.162-1.774,3.778-3.359,3.778S6.761,8.937,6.761,6.775 M3.415,17.69c0.218-3.51,3.142-6.297,6.704-6.297c3.562,0,6.486,2.787,6.705,6.297H3.415z"></path>
